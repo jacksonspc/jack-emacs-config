f8f0e8b2a876a888883bee8865335938c353484b
jacksonspc <archjacksontphone@gmail.com>
2022-03-06 14:11:01 -0800

    Add emacs stuff

---
+++ proj.el
@@ -0,0 +1,4 @@
+(setq projlist (list "/home/jack/Downloads/python-tut.org" "/home/jack/libj"))
+
+;; Do not edit by hand! Use proj-add! 
+(add-to-list 'projlist "/home/jack/.emacs.d/")(add-to-list 'projlist "/home/jack/.emacs.d/auto-save-list/")(add-to-list 'projlist "/home/jack/.emacs.d/")
